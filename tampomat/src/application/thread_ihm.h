16d6a33430fb81430727eb2ba3a82f427969db33
mikaeltrigo <trigo.mikael@gmail.com>
2013-05-09 20:28:04 +0200

    new file:   tampomat/src/application/thread_ihm.h

---
+++ tampomat/src/application/thread_ihm.h
@@ -0,0 +1,17 @@
+/*
+ * thread_ihm.h
+ *
+ *  Created on: 9 mai 2013
+ *      Author: R
+ */
+
+#ifndef THREAD_IHM_H_
+#define THREAD_IHM_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <includes.h>
+#include "stm32f10x_includes.h"
+#include "model.h"
+
+#endif /* THREAD_IHM_H_ */
