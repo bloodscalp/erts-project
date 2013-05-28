f0b24387ece7cb0f40ec620fe1297c3ed778365f
mikaeltrigo <trigo.mikael@gmail.com>
2013-05-28 15:02:44 +0200

    modified:   tampomat/src/application/thread_ihm.c

--- tampomat/src/application/thread_ihm.c
+++ tampomat/src/application/thread_ihm.c
@@ -32,51 +32,23 @@ void thread_ihm (void *p_arg)
 	(void)p_arg;
 	char cmd = '0';
 	char param[] = {'0','0', '0'};// paramètre pour le decomposer la commande de gaz ou break
-
-	//statusReg Status_on= on;
-
 	uint8_t flag = 0;
+	uint8_t flag2 = 0;
 //	uint8_t v = 0;
 //	uint8_t v0 = 0;
-	uint8_t t = 0;
-//	int8_t a = 0;
+//	uint8_t t = 0;
+//	uint8_t a = 0;
 
-	//---Global Variable------------------------------
-	//change plac
 	uint8_t throttle = 0;
 	uint8_t breaks = 0;
-//	uint8_t ACC =0;
-//	uint8_t DEC = 0;
-	//bool set=FALSE;
-	//bool reset=FALSE;
-	// attente de la commande de start pour démarrer
+
 	fprintf(fp_usart1,"S0\r\n");
-	while (cmd != '1') {
-		set_statusRegOn(off);
+	set_statusReg(off);
+	while(1){
 
 		// scrute les entrees
+		// les butont set reset et consor n'on aucun effet si on est sur off
 		if(USART1_CNT_IN > 0) {
-			cmd = USART1_BUFFER_IN[0];
-			USART1_CNT_IN = 0;
-		}
-
-		// Attente d'une seconde
-		OSTimeDly(OS_TICKS_PER_SEC / 1);
-	}
-	set_statusReg(on);
-
-	flag = 1;//on va triater l'entrée
-	//envois de la reponse de debut
-	//fprintf(fp_usart1," systeme enclenche\r\n");
-
-	// Attente d'une seconde
-	fprintf(fp_usart1,"S1\r\n");
-	OSTimeDly(OS_TICKS_PER_SEC / 10);
-
-	while (flag) {
-
-		// scrute les entrees
-		if(USART1_CNT_IN > 0){
 			cmd = USART1_BUFFER_IN[0];
 
 			if(cmd == 'A' || cmd == 'B'){
@@ -84,25 +56,27 @@ void thread_ihm (void *p_arg)
 				param[1] = USART1_BUFFER_IN[2];
 				param[2] = USART1_BUFFER_IN[3];
 			}
-
 			USART1_CNT_IN = 0;
+	//		if (cmd == '1'){
+	//			flag=1; //on est ON
+	//			flag2=1;
+	//		}
+
 		}
-		//fprintf(fp_usart1, "T%03u\r\n",(unsigned int)(get_throttle()));
-		//fprintf(fp_usart1, "V%03u\r\n",(unsigned int)(get_speed_sensor()));
+
+	if (flag ==1){
 		switch(cmd)
 		{
 			case '0' :
 				fprintf(fp_usart1, "S0\r\n");
+				set_statusReg(off);
 				OSTimeDly(OS_TICKS_PER_SEC / 10);
-
 				flag = 0;
 				break;
 			case 'R' :
 				fprintf(fp_usart1, "Reset ! \r\n");
 				set_cmd_res(TRUE);
 				OSTimeDly(OS_TICKS_PER_SEC / 10);
-
-
 				break;
 			case 'S':
 				fprintf(fp_usart1, "Set ! \r\n");
@@ -119,63 +93,70 @@ void thread_ihm (void *p_arg)
 				set_cmd_dec(TRUE);
 				OSTimeDly(OS_TICKS_PER_SEC / 10);
 				break;
-			case 'A' :
-				throttle = (param[0]-'0')*100+(param[1]-'0')*10+(param[2]-'0');
-
-				//fprintf(fp_usart1, "V%03u\r\n",(unsigned int)(throttle));
-				set_acc_sensor(throttle);
-				//set_throttle(throttle);
-				break;
-			case 'B' :
-				breaks = (param[0]-'0')*100+(param[1]-'0')*10+(param[2]-'0');
-				set_dec_sensor(breaks);
-				break;
 			default :
 				break;
 		}
+	}
+	switch(cmd)
+	{
+		case '1' :	flag=1;
+					flag2=1;
+				break;
+		case 'A' :
+			throttle = (param[0]-'0')*100+(param[1]-'0')*10+(param[2]-'0');
+			set_acc_sensor(throttle);
+				break;
+		case 'B' :
+			breaks = (param[0]-'0')*100+(param[1]-'0')*10+(param[2]-'0');
+			set_dec_sensor(breaks);
+				break;
+		default : break;
+	}
+	// Je gère ce qui va être envoyer àl'interface Java continuellement
+	if (cmd == 'A' || cmd == 'B') {
+		fprintf(fp_usart1, "T%03u\r\n",(unsigned int)(throttle));
+	}
+	else
+	{
+		fprintf(fp_usart1, "V%03u\r\n",(unsigned int)(get_speed_sensor()));
+	}
+	OSTimeDly(OS_TICKS_PER_SEC / 10);
+	if (flag2==1){
+		set_statusReg(on);
+		flag2=0;
+	}
 
-		// si on a accelere ou freine, on modifie l'acceleration
-		if (cmd == 'A' || cmd == 'B') {
-			//for debug
-			//fprintf(fp_usart1," THREAD IHM DETECTED THROTTLE\r\n");
-			fprintf(fp_usart1, "T%03u\r\n",(unsigned int)(throttle));
-		}
-		else
-		{
-			fprintf(fp_usart1, "V%03u\r\n",(unsigned int)(get_speed_sensor()));
-			//printf("%f",get_speed_sensor());
-		}
-		OSTimeDly(OS_TICKS_PER_SEC / 10);
-		// FIXME
-		switch (get_statusReg()) {
+	switch (get_statusReg()) {
 
-		case on:
-			fprintf(fp_usart1,"S1\r\n");
-			break;
+	case on:
 
-		case off:
-			fprintf(fp_usart1,"S0\r\n");
-			break;
+		fprintf(fp_usart1,"S1\r\n");
+		break;
 
-		case standby:
-			fprintf(fp_usart1,"SS\r\n");
+	case off:
+		fprintf(fp_usart1,"S0\r\n");
+		break;
 
-			break;
+	case standby:
+		fprintf(fp_usart1,"SS\r\n");
 
-		case interrupted:
-			fprintf(fp_usart1,"SI\r\n");
+		break;
 
-			break;
-		default :
-			break;
-		}
+	case interrupted:
+		fprintf(fp_usart1,"SI\r\n");
 
-		t++;
-		cmd = 1;
+		break;
+	default :
+		break;
+	}
 
-		// Attente d'une seconde
-		OSTimeDly(OS_TICKS_PER_SEC / 1);
+
+	cmd = 3;
+	OSTimeDly(OS_TICKS_PER_SEC / 5);
+
+
 	}
 }
+
 
 
